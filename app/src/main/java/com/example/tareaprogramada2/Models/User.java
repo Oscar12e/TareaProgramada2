c334058255f3f0b31f992a9af32dfda5d20ec3e2
Oscar Cortes <oscarcortesc12@gmail.com>
2019-10-25 03:05:51 -0600

    Initial commit

---
+++ app/src/main/java/com/example/tareaprogramada2/Models/User.java
@@ -0,0 +1,19 @@
+package com.example.tareaprogramada2.Models;
+
+import android.media.Image;
+
+import java.util.Date;
+import java.util.List;
+
+public class User {
+    public String _id;
+
+    public String name;
+    public String lastname;
+    public Image profilePic;
+    public List<String> education;
+    public Date birthday;
+    public String phoneNumber;
+    public String email;
+    public String sex;
+}
---
+++ app/src/main/java/com/example/tareaprogramada2/Presentations/RegisterActivity.java
@@ -0,0 +1,84 @@
+package com.example.tareaprogramada2.Presentations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.tareaprogramada2.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    public void registerNewUser(View v){
+        /*
+        if (!isDataValid()){
+            System.out.println("Dude what he fuck?");
+            return;
+        }
+
+        String nameText = name.getText().toString();
+        String lastnameText = lastname.getText().toString();
+        String emailText = email.getText().toString();
+        String passText = pass.getText().toString();
+
+        User user = new User();
+        user.name = nameText;
+        user.email = emailText;
+        user.lastName = lastnameText;
+        user.password = passText;
+
+        UserService service = RetrofitClient.getRetrofitInstance().create(UserService.class);
+
+        Call<ResponseBody> call = service.saveUser(user);
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                JSONObject message;
+                if (response.body() != null) {
+                    try {
+                        message = new JSONObject(response.body().string());
+                        String status = (String) message.get("operation");
+                        boolean sucessful = status.equals("sucessful");
+                        if (sucessful) {
+                            clearData();
+                            Toast.makeText(getBaseContext(), "Restaurante agregado con exito!" , Toast.LENGTH_SHORT ).show();
+                        } else {
+                            Toast.makeText(getBaseContext(), "Revise los datos" , Toast.LENGTH_SHORT ).show();
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                System.out.println("On failure statment");
+            }
+        });*/
+
+
+    }
+
+    public void cancel(View v){
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
